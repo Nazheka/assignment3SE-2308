a11bd182e3fb3e457f91ab0d2a712f17022fd60a
Nazheka <akezhanstudy@gmail.com>
2024-02-13 23:31:36 +0600

    started assignment

--- src/main/java/models/Person.java
+++ src/main/java/models/Person.java
@@ -1,4 +1,23 @@
 package models;
 
-public class Person {
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+
+public abstract class Person {
+    private long UIN;
+    private String surname;
+    private String name;
+    private String patronymic;
+    private boolean gender;
+    private String birthday;
+    private float weight;
+    private float height;
+
+
+
 }
