a5b433c90a3df83448d5c3621d98697ce455fde3
Asylzhan <asylzhan.kurmanbek@mail.ru>
2024-02-14 01:45:16 +0600

    zigazaga

--- src/main/java/models/Employee.java
+++ src/main/java/models/Employee.java
@@ -1,3 +1,5 @@
+pakage
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -5,6 +7,7 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
 public class Employee extends Person {
     private String employeeId;
     private String speciality;
@@ -12,16 +15,6 @@ public class Employee extends Person {
     private String appointmentTime;
     private double salary;
     private int vacationDays;
-
-    public Employee(String employeeId, String speciality, int workExperience, String appointmentTime, double salary, int vacationDays) {
-        super();
-        this.employeeId = employeeId;
-        this.speciality = speciality;
-        this.workExperience = workExperience;
-        this.appointmentTime = appointmentTime;
-        this.salary = salary;
-        this.vacationDays = vacationDays;
-    }
 
     public void increaseOfSalaryByWorkExperience() {
         int yearsOfExperience = workExperience / 12;
@@ -33,15 +26,4 @@ public class Employee extends Person {
         setSalary(newSalary);
     }
 
-    public void printEmployeeDetails() {
-        System.out.println("Employee Details:");
-        System.out.println("Name: " + getFirstName() + " " + getLastName());
-        System.out.println("Gender: " + getGender());
-        System.out.println("Employee ID: " + getEmployeeId());
-        System.out.println("Speciality: " + getSpeciality());
-        System.out.println("Work Experience: " + getWorkExperience() + " years");
-        System.out.println("Appointment Time: " + getAppointmentTime());
-        System.out.println("Salary: $" + getSalary());
-        System.out.println("Employee is on vacation for " + getVacationDays() + " days");
-    }
 }
